1a7183ab250793dc028e130cb4d41a851ba7ed2d
kravz2702-byte <kravz2702@gmail.com>
2021-09-09 23:13:42 +0300

    SQLAlchemy
    SQLAlchemy

--- Chapter1/config.py
+++ Chapter1/config.py
@@ -1,8 +1,12 @@
-class Config(object): 
-    pass 
- 
-class ProdConfig(Config): 
-    pass 
- 
-class DevConfig(Config): 
+
+class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
--- Chapter1/main.py
+++ Chapter1/main.py
@@ -1,13 +1,31 @@
+from enum import unique
 from flask import Flask
 from config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username):
+        self.username = username
+    
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
+        
 
 # Changed to show the git diff command
 @app.route('/')
 def home():
     return '<h1>Hello world</h1>'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
