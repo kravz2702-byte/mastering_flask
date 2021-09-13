eba36f997376e1efa0fa2a542e30641db0fd9d39
kravz2702-byte <kravz2702@gmail.com>
2021-09-13 22:23:28 +0300

    ralations
    many-to-many and one-to-many

--- Chapter1/manage.py
+++ Chapter1/manage.py
@@ -1,5 +1,6 @@
-from main import app, db, User
+from main import app, db, User, Post, Comment, Tag
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Post=Post, 
+                Comment=Comment, Tag=Tag)
