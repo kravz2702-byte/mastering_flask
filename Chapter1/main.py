d168b953bf25fd3f330910a93ac5b4ded994db8e
kravz2702-byte <kravz2702@gmail.com>
2021-09-14 23:00:43 +0300

    SQLAlchemy
    restrictions, constraints and migrations

--- Chapter1/main.py
+++ Chapter1/main.py
@@ -1,17 +1,21 @@
+from enum import unique
+from operator import index
+from os import name
 from flask import Flask
 from sqlalchemy.orm import backref
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True, nullable=False, index=True)
     password = db.Column(db.String(255))
     posts = db.relationship('Post', backref='user', lazy='dynamic')
 
@@ -23,16 +27,16 @@ class User(db.Model):
 
 
 tags = db.Table('post_tags',
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id', name='post_tag_id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id', name='tag_post_id'))
 )
 
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), nullable=False)
     text = db.Column(db.Text)
-    publish_date = db.Column(db.DateTime())
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', name='user_post'))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
     tags = db.relationship('Tag',
     secondary=tags,
@@ -47,7 +51,7 @@ class Post(db.Model):
 
 class Tag(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), unique=True, nullable=False)
 
     def __init__(self, title):
         self.title = title
@@ -57,10 +61,10 @@ class Tag(db.Model):
 
 class Comment(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255), nullable=False)
     text = db.Column(db.Text())
-    date = db.Column(db.DateTime())
-    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id', name='comment_post'))
 
     def __repr__(self):
         return "<Comment '{}'>".format(self.text[:15])
