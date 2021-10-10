b420adb79ae6ec55cda741ee2b6d2360b6b55847
kravz2702-byte <kravz2702@gmail.com>
2021-10-10 21:34:32 +0300

    new forms
    new forms for registration and login

--- Project1/webapp/auth/forms.py
+++ Project1/webapp/auth/forms.py
@@ -1,4 +1,5 @@
-from flask_wtf import FlaskForm as Form
+from flask_wtf import FlaskForm as Form, recaptcha
+from flask_wtf.recaptcha.fields import RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, URL 
 from .models import User
@@ -23,5 +24,24 @@ class LoginForm(Form):
         
         #Do the password match
         if not user.check_password(self.password.data):
-            self.password.errors.append
+            self.password.errors.append('Invalid username or password')
+        return True
+
+class RegisterForm(Form):
+    username = StringField('Username', [Length(255), DataRequired()])
+    password = PasswordField('Password', [DataRequired(), Length(min=8)])
+    confirm = PasswordField('Confirm Password', [DataRequired(), EqualTo('password')])
+    recaptcha = RecaptchaField()
+    def validate(self):
+        check_validate = super(RegisterForm, self).__init__()
+        #if our validators do not pass
+        if not check_validate:
+            return False
+        user = User.query.filter_by(self.username.data).first()
+        #if the username already being used
+        if user:
+            self.username.errors.append(
+                'USer with this name already exists'
+            )
+            return False
         return True
