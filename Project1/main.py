5cc259650085633fa508ce2e706c022201f6706b
kravz2702-byte <kravz2702@gmail.com>
2021-11-02 22:21:33 +0300

    End of Chapter
    Decided to remove all files and download them from git repo

--- Project1/main.py
+++
@@ -1,9 +0,0 @@
-import os 
-from webapp import create_app
-from config import DevConfig
-
-#env = os.environ.get('WEBAPP_ENV', 'dev')
-app = create_app(DevConfig)
-
-if __name__ == '__main__':
-    app.run(debug=True)
